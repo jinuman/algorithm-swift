112bf545c68e9f9ef77f388943fd1df058b89ab5
jinuman <jin35kim@gmail.com>
2019-01-30 19:00:59 +0900

    solve: Programmers 나머지 한 점

---
+++ Kakao/P_LastPoint.swift
@@ -0,0 +1,19 @@
+// 나머지 한 점
+// 직사각형의 세 점이 주어지고 나머지 한 점을 구하는 문제
+
+import Foundation
+
+class P_LastPoint {
+    func solution(_ v: [[Int]]) -> [Int] {
+        let xArr: [Int] = v.map { $0[0] }
+        let yArr: [Int] = v.map { $0[1] }
+        let x: Int = xArr.reduce(0, ^)
+        let y: Int = yArr.reduce(0, ^)
+        return [x, y]
+    }
+
+    func run() {
+        print(solution([[1, 4], [3, 4], [3, 10]]))  // [1, 10]
+        print(solution([[1, 1], [2, 2], [1, 2]]))   // [2, 1]
+    }
+}
