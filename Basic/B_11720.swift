d6bf05552ba20c14297392ae8c0a37f0afe03e39
jinuman <jin35kim@gmail.com>
2019-02-02 21:53:50 +0900

    solve: Baekjoon 11720

---
+++ Basic/B_11720.swift
@@ -0,0 +1,26 @@
+// 숫자의 합
+// compactMap, reduce 활용
+
+import Foundation
+
+class B_11720 {
+    func run() {
+        guard
+                let _ = readLine(),
+                let line = readLine()
+                else { return }
+        let numbers = Array(line).compactMap { Int(String($0)) }
+        print(numbers.reduce(0, +))
+    }
+}
+
+//B_11720().run()
+
+/*
+input
+5
+54321
+
+output
+15
+*/
