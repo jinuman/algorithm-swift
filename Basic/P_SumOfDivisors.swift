52e67b921850ffa94e28b9f2f7444c20a99bdee9
jinuman <jin35kim@gmail.com>
2019-02-03 20:42:27 +0900

    solve: Programmers 약수의 합

---
+++ Basic/P_SumOfDivisors.swift
@@ -0,0 +1,20 @@
+// 약수의 합
+// for 문을 n/2만 돌리는 방법
+
+import Foundation
+
+class P_SumOfDivisors {
+    func solution(_ n: Int) -> Int {
+        var sum: Int = 0
+        for i in stride(from: 1, through: n/2, by: 1) {
+            if n % i == 0 {
+                sum += i
+            }
+        }
+        return sum + n
+    }
+    func run() {
+        print(solution(12)) // 28
+        print(solution(5))  // 6
+    }
+}
