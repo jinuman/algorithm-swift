3d03d360bfa5a84e86712c190c1267778e91ec8e
jinuman <jin35kim@gmail.com>
2019-02-09 12:56:41 +0900

    solve: Baekjoon 1152

---
+++ String/B_1152.swift
@@ -0,0 +1,21 @@
+// 단어의 개수
+// split 함수는 디폴트로 공백을 생략한다. omittingEmptySubsequences = true
+
+import Foundation
+
+class B_1152 {
+    func run() {
+        guard let input = readLine() else { return }
+        print(input.split(separator: " ").count)
+    }
+}
+
+//B_1152().run()
+
+/*
+input
+ Mazatneunde Wae Teullyeoyo
+
+output
+3
+*/
