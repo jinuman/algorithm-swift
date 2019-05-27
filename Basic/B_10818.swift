e960d9540efa1d2c9a74b11075cf3cc16901628a
jinuman <jin35kim@gmail.com>
2019-05-27 22:51:58 +0900

    refactor: Baekjoon 10818

--- Basic/B_10818.swift
+++ Basic/B_10818.swift
@@ -4,21 +4,19 @@ import Foundation
 
 class B_10818 {
     func run() {
-        if let _ = readLine() {
-            guard let line = readLine() else {
-                return
-            }
-            let array: [Int] = line.components(separatedBy: " ").map {
-                Int($0)!
-            }
-            var max: Int = -1000000
-            var min: Int = 1000000
-            for item in array {
-                max = (item > max) ? item : max
-                min = (item < min) ? item : min
-            }
-            print(min, max)
+        guard
+            let _ = readLine(),
+            let line = readLine()?.components(separatedBy: " ") else { return }
+        
+        let array: [Int] = line.compactMap { Int($0) }
+        
+        var max: Int = -1000000
+        var min: Int = 1000000
+        for item in array {
+            max = (item > max) ? item : max
+            min = (item < min) ? item : min
         }
+        print(min, max)
     }
 }
 
