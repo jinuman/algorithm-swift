1f30ab848fa217b29d14095c6aa2f8fbba9aada1
jinuman <jin35kim@gmail.com>
2019-05-27 22:52:06 +0900

    refactor: Baekjoon 10824

--- Basic/B_10824.swift
+++ Basic/B_10824.swift
@@ -4,10 +4,10 @@ import Foundation
 
 class B_10824 {
     func run() {
-        let array: [String] = readLine()!.components(separatedBy: " ")
+        guard let array: [String] = readLine()?.components(separatedBy: " ") else { return }
         let str1 = array[0] + array[1]
         let str2 = array[2] + array[3]
-        print(Int(str1)! + Int(str2)!)
+        print((Int(str1) ?? 0) + (Int(str2) ?? 0))
     }
 }
 
