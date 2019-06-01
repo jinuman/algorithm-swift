8a66fe3aaba7d38543bc029e260526f99358aad8
jinuman <jin35kim@gmail.com>
2019-06-01 23:55:55 +0900

    refactor: Baekjoon 1924

--- Implementation/B_1924.swift
+++ Implementation/B_1924.swift
@@ -7,10 +7,9 @@ class B_1924 {
         let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
         let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         var totalDays: Int = 0
-
-        let date = readLine()!.components(separatedBy: " ").map {
-            Int($0)!
-        }
+        
+        guard let inputs = readLine()?.components(separatedBy: " ") else { return }
+        let date = inputs.compactMap { Int($0) }
         let month = date[0]
         let day = date[1]
         totalDays = months[0..<month - 1].reduce(0, { $0 + $1 }) + day
