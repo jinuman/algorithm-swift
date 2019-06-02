9e1f06942f98ea221403fd2675278454b6cd1234
jinuman <jin35kim@gmail.com>
2019-06-02 16:42:03 +0900

    refactor: Baekjoon 10872

--- Math/B_10872.swift
+++ Math/B_10872.swift
@@ -4,9 +4,8 @@ import Foundation
 
 class B_10872 {
     func run() {
-        if let num: Int = Int(readLine()!) {
-            print((1...max(num, 1)).reduce(1, *))
-        }
+        guard let num: Int = Int(readLine() ?? "0") else { return }
+        print((1...max(num, 1)).reduce(1, *))
     }
 }
 
