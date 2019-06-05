d916288735811ca7e758c0a1f61edd13005e19d5
jinuman <jin35kim@gmail.com>
2019-06-05 13:27:37 +0900

    refactor: Baekjoon 2745

--- Math/B_2745.swift
+++ Math/B_2745.swift
@@ -16,20 +16,18 @@ extension Int {
 class B_2745 {
     func run() {
         let arr = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ").map({ String($0) })
-        guard let input = readLine()?.split(separator: " ").map({ String($0) }) else {
-            return
-        }
+        guard let input = readLine()?.split(separator: " ").map({ String($0.uppercased()) }) else { return }
+        
         let N: String = input[0]
-        let len: Int = N.count
         let B: Int = Int(input[1]) ?? 0
-        var tmp: Int = len - 1     // 앞에서 부터 계산
+        
+        var exponential: Int = N.count - 1     // 앞에서 부터 계산
         var answer: Int = 0
+        
         N.forEach {
-            guard let num: Int = arr.firstIndex(of: String($0)) else {
-                return
-            }
-            answer += num * B.power(tmp)
-            tmp -= 1
+            guard let num: Int = arr.firstIndex(of: String($0)) else { return }
+            answer += num * B.power(exponential)
+            exponential -= 1
         }
         print(answer)
     }
