bcaf1f98f9e4eaac8fc90d52656b3f76b1004731
jinuman <jin35kim@gmail.com>
2019-06-10 00:01:00 +0900

    refactor: Baekjoon 10828

--- Stack/B_10828.swift
+++ Stack/B_10828.swift
@@ -28,58 +28,57 @@ class Stack {
 
 class B_10828 {
     func run() {
-        if let n = Int(readLine() ?? "0") {
-            let stack = Stack()
-            
-            for _ in 0..<n {
-                guard let cmd = readLine()?.split(separator: " ") else { break }
-                switch cmd[0] {
-                case "push":
-                    stack.push(Int(cmd[1]) ?? 0)
-                case "pop":
-                    print(stack.pop() ?? -1)
-                case "top":
-                    print(stack.top() ?? -1)
-                case "empty":
-                    print(stack.empty())
-                case "size":
-                    print(stack.size())
-                default:
-                    break
-                }
+        guard let n = Int(readLine() ?? "0") else { return }
+        let stack = Stack()
+        
+        for _ in 0..<n {
+            guard let cmd = readLine()?.split(separator: " ") else { break }
+            switch cmd[0] {
+            case "push":
+                stack.push(Int(cmd[1]) ?? 0)
+            case "pop":
+                print(stack.pop() ?? -1)
+            case "top":
+                print(stack.top() ?? -1)
+            case "empty":
+                print(stack.empty())
+            case "size":
+                print(stack.size())
+            default:
+                break
             }
         }
     }
 }
 
 /*
- input
- 14
- push 1
- push 2
- top
- size
- empty
- pop
- pop
- pop
- size
- empty
- pop
- push 3
- empty
- top
+input
+14
+push 1
+push 2
+top
+size
+empty
+pop
+pop
+pop
+size
+empty
+pop
+push 3
+empty
+top
  
- output
- 2
- 2
- 0
- 2
- 1
- -1
- 0
- 1
- -1
- 0
- 3
- */
+output
+2
+2
+0
+2
+1
+-1
+0
+1
+-1
+0
+3
+*/
