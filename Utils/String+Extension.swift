8a6e768de48318dd9e569e7b4a6b8aa6994bd134
jinuman <jin35kim@gmail.com>
2019-09-16 01:35:38 +0900

    Add String+Extension

---
+++ Utils/String+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  String+Extension.swift
+//  AlgorithmSwift
+//
+//  Created by Jinwoo Kim on 16/09/2019.
+//  Copyright © 2019 Jinuman. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    subscript (i: Int) -> Character {
+        return self[self.index(self.startIndex, offsetBy: i)]
+    }
+    
+    // for convenience we should include String return
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = self.index(self.startIndex, offsetBy: r.lowerBound)
+        let end = self.index(self.startIndex, offsetBy: r.upperBound)
+        
+        return String(self[start ..< end])
+    }
+    
+    subscript (r: ClosedRange<Int>) -> String {
+        let start = self.index(self.startIndex, offsetBy: r.lowerBound)
+        let end = self.index(self.startIndex, offsetBy: r.upperBound)
+        
+        return String(self[start ... end])
+    }
+    
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
