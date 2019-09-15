b4221e615862b50985f75b198538771e004412dc
jinuman <jin35kim@gmail.com>
2019-09-15 21:36:21 +0900

    Add BubbleSort

---
+++ Concept/BubbleSort.swift
@@ -0,0 +1,34 @@
+//
+//  BubbleSort.swift
+//  AlgorithmSwift
+//
+//  Created by Jinwoo Kim on 29/08/2019.
+//  Copyright © 2019 Jinuman. All rights reserved.
+//
+
+import Foundation
+
+class BubbleSort {
+    
+    let arr: [Int] = [3, 5, 4, 2, 9, 6, 10, 8, 7, 1]
+    
+    func bubbleSort(arr: [Int]) -> [Int] {
+        var newArr: [Int] = arr
+        let n: Int = newArr.count
+        
+        for i in (0 ..< n - 1) {
+            for j in (0 ..< n - i - 1) {
+                if newArr[j] > newArr[j + 1] {
+                    newArr.swapAt(j, j + 1)
+                }
+            }
+        }
+        
+        return newArr
+    }
+    
+    func run() {
+        print(bubbleSort(arr: arr))
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+}
