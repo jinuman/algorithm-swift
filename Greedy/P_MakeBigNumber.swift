73ecb91376874954ed09a13ac62e6b75efcbaa01
jinuman <jin35kim@gmail.com>
2019-09-16 01:36:01 +0900

    Solve P_MakeBigNumber

--- Greedy/P_MakeBigNumber.swift
+++ Greedy/P_MakeBigNumber.swift
@@ -1,34 +1,35 @@
 // 큰 수 만들기
 
-#warning("나중에 해보자..String subscript 사용해보자..")
-
 import Foundation
 
 class P_MakeBigNumber {
     func solution(_ number: String, _ k: Int) -> String {
-        let digit = number.count - k
+        let size = number.count - k
+        var count = k
+        var stack: [String] = []
         
-        let newNumber: Int = Int(number) ?? -1
-        
-        var max: Character = '0'
-        
-        if digit == 0 {
-            return number
-        }
-        
-        var idx: Int = 0
-        for i in 0..<digit {
-            for j in idx...(k + i) {
-//                if max < number...
+        number.forEach { element in
+            while true {
+                guard
+                    let peek = stack.last,
+                    peek < element.description else { break }
+                
+                if count == 0 { break }
+                count -= 1
+                
+                _ = stack.popLast()
             }
+            stack.append(element.description)
         }
         
-        return ""
+        return stack.joined()[0 ..< size]
     }
+    
     
     func run() {
         print(solution("1924", 2))       // 94
         print(solution("1231234", 3))    // 3234
         print(solution("4177252841", 4)) // 775841
+        print(solution("10000", 2))      // 100
     }
 }
