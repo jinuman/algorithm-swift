279efba7e066254580535f1e0a5a13593e504299
jinuman <jin35kim@gmail.com>
2019-09-15 21:36:40 +0900

    Solve PermMissingElem

---
+++ Codility/PermMissingElem.swift
@@ -0,0 +1,18 @@
+// Task 03 : Time Complexity
+
+import Foundation
+
+class PermMissingElem {
+    func solution(_ A: inout [Int]) -> Int {
+        let sum = (0 ... A.count + 1).reduce(0, +)
+        let sumOfA = A.reduce(0, +)
+        return sum - sumOfA
+    }
+    
+    func run() {
+        var testArray1 = [2, 3, 1, 5]
+        var testArray2 = [3, 4, 1, 5]
+        print(solution(&testArray1))  // 4
+        print(solution(&testArray2))  // 2
+    }
+}
