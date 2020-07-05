456aa1536e3761c4736b8fcb3354240528a5c0d7
Jinwoo Kim <jin35kim@gmail.com>
2020-07-05 12:58:49 +0900

    fix: Update BinaryGap solving

--- AlgorithmSwift/Codility/BinaryGap.swift
+++ AlgorithmSwift/Codility/BinaryGap.swift
@@ -10,14 +10,14 @@ class BinaryGap {
         var number = N
         var maxBinaryGap: Int = 0
         var countBinaryGap: Int = 0
-        var countFlag: Bool = false
+        var isFirstOneAppeared: Bool = false
         
         while number != 0 {
             if number & 1 == 1 {  // 자리수가 1이면
-                if !countFlag {
-                    countFlag = true
+                if !isFirstOneAppeared {
+                    isFirstOneAppeared = true
                 } else {
-                    // 최초의 1이 나온 후부터 계산을 적용한다.
+                    // 최초의 1이 나온 후부터 계산
                     maxBinaryGap = max(maxBinaryGap, countBinaryGap)
                 }
                 countBinaryGap = 0
@@ -31,13 +31,14 @@ class BinaryGap {
     
     // 함수형 버전
     func solution2(_ N: Int) -> Int {
-        let cs0 = CharacterSet.init(charactersIn: "0")
-        let cs1 = CharacterSet.init(charactersIn: "1")
+        let zero = CharacterSet.init(charactersIn: "0")
+        let one = CharacterSet.init(charactersIn: "1")
         let binaryGap = String(N, radix: 2)
-            .trimmingCharacters(in: cs0)
-            .trimmingCharacters(in: cs1)
+            .trimmingCharacters(in: zero)
+            .trimmingCharacters(in: one)
             .split(separator: "1")
-            .max()?.count
+            .max()?
+            .count
         
         guard let gap = binaryGap else {
             return 0
@@ -51,7 +52,7 @@ class BinaryGap {
         print(solution1(32))  // 0
         print(solution1(17))  // 3
         
-        print(solution2(15))  // 0
+        print(solution2(1))  // 0
         print(solution2(13))  // 1
         print(solution2(32))  // 0
         print(solution2(17))  // 3
--- AlgorithmSwift/main.swift
+++ AlgorithmSwift/main.swift
@@ -1,3 +1,3 @@
 import Foundation
 
-B_10826().run()
+BinaryGap().run()
