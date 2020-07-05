5a34936b5f730c7eef03806f9224002bf6ef4f33
Jinwoo Kim <jin35kim@gmail.com>
2020-07-05 18:21:27 +0900

    fix: Update OddOccurrencesInArray solving

--- AlgorithmSwift/Codility/OddOccurrencesInArray.swift
+++ AlgorithmSwift/Codility/OddOccurrencesInArray.swift
@@ -1,4 +1,5 @@
 // Lesson 02 : Arrays
+// all but one of the values in A occur an even number of times.
 
 // 배열 A 의 모든 원소를 xor 비트연산 시킬 경우 짝이 없는 숫자만 남게된다.
 
@@ -11,6 +12,6 @@ class OddOccurrencesInArray {
     
     func run() {
         var testArray = [9, 3, 9, 3, 9, 7, 9]
-        print(solution(&testArray))  // 7
+        print(self.solution(&testArray))  // 7
     }
 }
