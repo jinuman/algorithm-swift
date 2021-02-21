ed112fbcfffab93da963496f5ebda62b65c510fa
Jinwoo Kim <jinumandev@gmail.com>
2021-02-22 02:03:38 +0900

    프로젝트 폴더 정리

---
+++ AlgorithmSwift/Temporary/Test.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+final class Test {
+    func run() {
+        let arr: [Int] = [1, 3, 3]
+        let unpairedNumber = arr.reduce(0, ^)
+        print(unpairedNumber) // 1
+    }
+}
+
+final class P_Level1_1 {
+    func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+        var answer = [Int]()
+        arr.forEach {
+            if $0 % divisor == 0 {
+                answer.append($0)
+            }
+        }
+        return answer.isEmpty ? [-1] : answer.sorted()
+    }
+
+    func run() {
+        print(solution([5, 9, 7, 10], 5)) // [5, 10]
+        print(solution([2, 36, 1, 3], 1)) // [1, 2, 3, 36]
+        print(solution([3, 2, 6], 10)) // [-1]
+    }
+}
--- AlgorithmSwift/main.swift
+++ AlgorithmSwift/main.swift
@@ -1,3 +1,7 @@
 // MARK: - main
 
 P_BuyingCookies().run()
+
+Test().run()
+
+P_Level1_1().run()
