663f1de4f1d090c1cb0c6da1dc2526fa1961a07d
Jinwoo Kim <jinumandev@gmail.com>
2021-02-23 16:12:13 +0900

    refactor: Codility BinaryGap

--- AlgorithmSwift/Codility/BinaryGap.swift
+++ AlgorithmSwift/Codility/BinaryGap.swift
@@ -2,13 +2,16 @@
 
 /*
  Maximal Sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+ Longest binary gap(1 과 1 사이의 0이 몇개인지) 구하는 문제
+ given N: 529 = binary: 1000010001
+ return 4
  */
 
 import Foundation
 
 struct BinaryGap {
     // 일반 버전
-    func solution1(_ N: Int) -> Int {
+    func solution(_ N: Int) -> Int {
         var number = N
         var maxBinaryGap: Int = 0
         var countBinaryGap: Int = 0
@@ -32,32 +35,32 @@ struct BinaryGap {
     }
     
     // 함수형 버전
-    func solution2(_ N: Int) -> Int {
-        let zero = CharacterSet.init(charactersIn: "0")
-        let one = CharacterSet.init(charactersIn: "1")
-        let binaryGap = String(N, radix: 2)
+    func functionalSolution(_ N: Int) -> Int {
+        let zero = CharacterSet(charactersIn: "0")
+        let one = CharacterSet(charactersIn: "1")
+        let binaryGapCount = String(N, radix: 2)
             .trimmingCharacters(in: zero)
             .trimmingCharacters(in: one)
             .split(separator: "1")
             .max()?
             .count
         
-        guard let gap = binaryGap else {
+        guard let answer = binaryGapCount else {
             return 0
         }
-        return gap
+        return answer
     }
     
     func run() {
-        print(solution1(15))  // 0
-        print(solution1(13))  // 1
-        print(solution1(32))  // 0
-        print(solution1(17))  // 3
+        print(solution(15))  // 0
+        print(solution(13))  // 1
+        print(solution(32))  // 0
+        print(solution(17))  // 3
         
-        print(solution2(1))  // 0
-        print(solution2(13))  // 1
-        print(solution2(32))  // 0
-        print(solution2(17))  // 3
-        print(solution2(580)) // 3
+        print(functionalSolution(1))  // 0
+        print(functionalSolution(13))  // 1
+        print(functionalSolution(32))  // 0
+        print(functionalSolution(17))  // 3
+        print(functionalSolution(529)) // 4
     }
 }
