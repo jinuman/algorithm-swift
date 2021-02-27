4e2e8d2c38f8cbb8b6d2e7cfa193327264bbe6ed
Jinwoo Kim <jinumandev@gmail.com>
2021-02-27 15:49:48 +0900

    refactor: Baekjoon 10808 알파벳 개수

--- AlgorithmSwift/Baekjoon/String/B_10808.swift
+++ AlgorithmSwift/Baekjoon/String/B_10808.swift
@@ -1,26 +1,35 @@
 // 알파벳 개수
+
+/* 문제
+ 알파벳 소문자로만 이루어진 단어 S가 주어진다. 각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램을 작성하시오.
+ */
+
 // firstIndex(of:), joined(separator:)
 
 import Foundation
 
-class B_10808 {
+struct B_10808 {
+    func solution(_ input: String) -> String {
+        let alphabets = Array("abcdefghijklmnopqrstuvwxyz")
+        var check = [Int](repeating: 0, count: alphabets.count)
+        for element in input {
+            guard let index = alphabets.firstIndex(of: element) else { break }
+            check[index] += 1
+        }
+        let answer = check.map { $0.description }.joined(separator: " ")
+        return answer
+    }
+
     func run() {
         guard let input = readLine() else { return }
-        let alphabets = Array("abcdefghijklmnopqrstuvwxyz")
-        var result = [Int].init(repeating: 0, count: alphabets.count)
-        input.forEach { ch in
-            guard let idx = alphabets.firstIndex(of: ch) else { return }
-            result[idx] += 1
-        }
-        print(result.map { String($0) }
-            .joined(separator: " "))
+        print(solution(input))
     }
 }
 
 /*
-input
-baekjoon
+ input
+ baekjoon
 
-output
-1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
-*/
+ output
+ 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
+ */
