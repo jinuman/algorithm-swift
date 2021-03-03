8a2e2e1b13b5275cf0cd2953f78b7ad7e21d4863
Jinwoo Kim <jinumandev@gmail.com>
2021-03-03 10:51:40 +0900

    refactor: Programmers Budget

--- AlgorithmSwift/Programmers/SummerWinterCoding/P_Budget.swift
+++ AlgorithmSwift/Programmers/SummerWinterCoding/P_Budget.swift
@@ -1,18 +1,25 @@
 // 예산
-// 2018년
+
+/* 문제
+ 부서별로 신청한 금액이 들어있는 배열 d, 예산 budget
+ 최대로 지원할 수 있는 부서의 수 return
+
+ For example,
+ d: [4, 2, 3, 3], budget: 10 -> 4개의 부서에 지원하기엔 예산이 부족함 -> return 3
+ */
 
 import Foundation
 
-final class P_Budget {
+struct P_Budget {
     func solution(_ d: [Int], _ budget: Int) -> Int {
-        let sortedNumbers = d.sorted(by: <)
+        let sortedAmounts = d.sorted(by: <)
         var answer = 0
-        var amount = 0
-        for number in sortedNumbers {
-            if amount + number > budget {
+        var sum = 0
+        for amount in sortedAmounts {
+            if sum + amount > budget {
                 break
             }
-            amount += number
+            sum += amount
             answer += 1
         }
         return answer
@@ -20,6 +27,7 @@ final class P_Budget {
 
     func run() {
         print(solution([1, 3, 2, 5, 4], 9)) // 3
-        print(solution([2, 2, 3, 3], 10))  // 4
+        print(solution([2, 2, 3, 3], 10)) // 4
+        print(solution([4, 2, 3, 3], 10)) // 3
     }
 }
