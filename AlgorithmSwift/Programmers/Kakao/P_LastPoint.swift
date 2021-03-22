ea7a0a22ab07889b96fe772fa41f6eb7d5ac0b56
Jinwoo Kim <jinumandev@gmail.com>
2021-03-23 00:57:29 +0900

    refactor: Programmers 나머지 한 점

--- AlgorithmSwift/Programmers/Kakao/P_LastPoint.swift
+++ AlgorithmSwift/Programmers/Kakao/P_LastPoint.swift
@@ -1,18 +1,18 @@
 // 나머지 한 점
-// 직사각형의 세 점이 주어지고 나머지 한 점을 구하는 문제
+
+/* 문제
+ 직사각형의 세 점이 주어질 때 나머지 한 점의 좌표를 return
+ */
 
 import Foundation
 
-class P_LastPoint {
+final class P_LastPoint {
     func solution(_ v: [[Int]]) -> [Int] {
-        let xArr: [Int] = v.map {
-            $0[0]
-        }
-        let yArr: [Int] = v.map {
-            $0[1]
-        }
-        let x: Int = xArr.reduce(0, ^)
-        let y: Int = yArr.reduce(0, ^)
+        let xList: [Int] = v.map { $0[0] } // [1, 3, 3]
+        let yList: [Int] = v.map { $0[1] } // [4, 4, 10]
+
+        let x: Int = xList.reduce(0, ^)
+        let y: Int = yList.reduce(0, ^)
         return [x, y]
     }
 
