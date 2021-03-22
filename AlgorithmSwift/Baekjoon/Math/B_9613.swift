dc11e3b3e4fdf6a169a1125160817fd93e6e4e55
Jinwoo Kim <jinumandev@gmail.com>
2021-03-22 22:10:49 +0900

    refactor: Baekjoon 9613

--- AlgorithmSwift/Baekjoon/Math/B_9613.swift
+++ AlgorithmSwift/Baekjoon/Math/B_9613.swift
@@ -1,9 +1,21 @@
 // GCD 합
 
+/* 문제
+ GCD: 최대 공약수
+ 양의 정수 n 개가 주어질 때, 가능한 모든 쌍의 GCD 합을 구하시오.
+
+ For example,
+ 3(n) 2 6 12 이면,
+ - gcd(2, 6) = 2
+ - gcd(2, 12) = 2
+ - gcd(6, 12) = 6
+ 총 10 return
+ */
+
 import Foundation
 
-class B_9613 {
-    private func gcd(_ num1: Int, _ num2: Int) -> Int {
+struct B_9613 {
+    private func iterativeGCD(_ num1: Int, _ num2: Int) -> Int {
         var a = num1
         var b = num2
         while b != 0 {
@@ -14,35 +26,42 @@ class B_9613 {
         return a
     }
 
+    private func recursiveGCD(_ a: Int, _ b: Int) -> Int {
+        if b == 0 {
+            return a
+        }
+        return recursiveGCD(b, a % b)
+    }
+
     func run() {
-        guard let testcase = Int(readLine() ?? "0") else { return }
-        
-        for _ in 0..<testcase {
-            guard let caseLine = readLine()?.split(separator: " ") else { return }
-            var arr: [Int] = caseLine.compactMap { Int($0) }
-            let n = arr.first ?? -1
-            arr.removeFirst()
-            var result = 0
-            
+        let testcase = Int(readLine()!)!
+
+        for _ in 0 ..< testcase {
+            var numbers: [Int] = readLine()!.components(separatedBy: " ").compactMap { Int($0) }
+            let n = numbers.removeFirst()
+            var answer = 0
+
             for i in stride(from: 0, to: n - 1, by: 1) {
                 for j in stride(from: i + 1, to: n, by: 1) {
-                    result += gcd(arr[i], arr[j])
+                    answer += recursiveGCD(numbers[i], numbers[j])
                 }
             }
-            print(result)
+            print(answer)
         }
     }
 }
 
 /*
 input
-3
+4
 4 10 20 30 40
 3 7 5 12
 3 125 15 25
+3 2 6 12
 
 output
 70
 3
 35
+10
 */
